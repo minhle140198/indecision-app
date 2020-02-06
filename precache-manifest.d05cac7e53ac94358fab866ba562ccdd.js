4fe2186f223bc1b8d058583fa3712a38f688107c
minh le 1998 <minh.le@kyanon.digital>
2020-02-06 14:43:04 +0700

    Updates

---
+++ precache-manifest.d05cac7e53ac94358fab866ba562ccdd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a111cf3626f6918327e3da4593f16065",
+    "url": "/indecision-app/index.html"
+  },
+  {
+    "revision": "fc6fadbabcc93dba7774",
+    "url": "/indecision-app/static/css/main.ef7bc4a8.chunk.css"
+  },
+  {
+    "revision": "8ceb0a2775f1aa1f8806",
+    "url": "/indecision-app/static/js/2.77e9ab8b.chunk.js"
+  },
+  {
+    "revision": "fc6fadbabcc93dba7774",
+    "url": "/indecision-app/static/js/main.0e434a56.chunk.js"
+  },
+  {
+    "revision": "23eb2b8d37094dd89a44",
+    "url": "/indecision-app/static/js/runtime~main.38f46e93.js"
+  }
+]);
